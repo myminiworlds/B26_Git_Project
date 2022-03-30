f1b06f62b12b706a8a3970c14f7548f3df3d180e
Seniye Kaya <seniyeerdinckaya@gmail.com>
2022-03-29 21:17:39 -0400

    updated test case 001

---
+++ src/day0/Jamesbond.java
@@ -0,0 +1,5 @@
+ package day0;
+public class  Jamesbond{
+  public static void main(String[] args) {
+    System.out.println("James bonds");
+  }}
--- src/day0/new file.java
+++
@@ -1,5 +0,0 @@
- package day0;
-public class  Jamesbond
-  public static void main(String[] args){
-System.out.println("James bOns")
-}
